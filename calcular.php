82990d444ccc3d20999877215e544b391be103cd
vitorpedroso283 <vitor.pedroso@multilaser.com.br>
2023-01-28 12:14:00 -0300

    first commit

---
+++ calcular.php
@@ -0,0 +1,39 @@
+<?php
+
+	require_once 'classes/Calculadora.php';
+
+	$numero1 = $_POST['numero1'];
+	$numero2 = $_POST['numero2'];
+	echo $operacao = $_POST['operacao'];
+
+	$calculadora = new Calculadora();
+
+	//Setar Valores
+	$calculadora -> setNumero1($numero1);
+	$calculadora -> setNumero2($numero2);
+
+	//operacoes
+	
+
+	switch ($operacao) {
+		case 'somar':
+			$calculadora->somar();
+			break;
+
+		case 'subtrair':
+			$calculadora->subtrair();
+			break;
+
+		case 'multiplicar':
+			$calculadora->multiplicar();
+			break;
+
+		case 'dividir':
+			$calculadora->dividir();
+			break;
+		
+	}
+
+	echo $calculadora->getTotal();
+
+?>
